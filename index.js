0c030b35ba70d37f421cb274f2efb219d02723c4
adeluigi <ademario63@gmail.com>
2021-06-05 11:31:35 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose');
 const routes = require('./src/routes/routes')
 const app = express();
-
+app.use(cors())
 mongoose.connect('mongodb+srv://admin:admin@cluster0.ws0tq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
